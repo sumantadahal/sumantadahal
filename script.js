60344f77ccc52a0fc883a91908f114311094ee80
sumantadahal <www.saugatpokharel@gmail.com>
2024-06-17 17:22:42 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Student","Sports enthusiast","Naturephile","Selenophile","Astrophile"],
+        typeSpeed: 50,
+        backSpeed: 60,
+        loop: true,
+    });
+ 
+    var typed = new Typed(".typing-2",{
+        strings: ["Student","Sports enthusiast","Naturephile","Selenophile","Astrophile"],
+        typeSpeed: 50,
+        backSpeed: 60,
+        loop: true,
+    });
+});
+
+     // Scroll to top button script
+      const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+      btnScrollToTop.addEventListener("click", function(){
+        window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    });
